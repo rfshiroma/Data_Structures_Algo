f932c18cea6617a0ef7712e23b56207520892e37
ryan_shiroma@hotmail.com <ryan_shiroma@hotmail.com>
2020-09-28 09:47:28 -0300

    Added files via upload

--- Trees/Binary_Tree.py
+++ Trees/Binary_Tree.py
@@ -6,15 +6,36 @@ class BinaryTree(Tree):
     '''Abstract base class representing a binary tree structure.'''
 
     # ---------------- additional abstract methods ----------------
-    def left():
-        pass
+    def left(self, p):
+        '''Return a Position representing p's left child.
 
-    def right():
-        pass
+        Return None if p does not have a left child.
+        '''
+        raise NotImplementedError('must be implemented by subclass')
+
+    def right(self, p):
+        '''Return a Position representing p's right child.
+
+        Return None if p does not have a right child.
+        '''
+        raise NotImplementedError('must be implemented by subclass')
+
 
     # ---------------- concrete methods implemented ----------------
-    def siblings():
-        pass
+    def siblings(self, p):
+        '''Return a Position representing p's sibling (or None if no sibling).'''
+        parent = self.parent(p)
+        if parent is None:                          # p must be the root
+            return None                             # root has no sibling
+        else:
+            if p == self.right(parent):
+                return self.right(parent)           # possibly None
+            else:
+                return self.left(parent)            # possibly None
 
-    def children():
-        pass
+    def children(self, p):
+        '''Generate an iteration of Positions representing p's children.'''
+        if self.left(p) is not None:
+            yield self.left(p)
+        if self.right(p) is not None:
+            yield self.right(p)
