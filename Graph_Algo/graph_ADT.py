1e2a4592492e0d8cc446109c123ef2aa24ccfa2d
ryan_shiroma@hotmail.com <ryan_shiroma@hotmail.com>
2020-09-30 19:50:50 -0300

    Added files via upload

--- Graph_Algo/graph_ADT.py
+++ Graph_Algo/graph_ADT.py
@@ -4,6 +4,7 @@
 # Usage of adjacency map representation
 
 # ---------------- nested Vertex class ----------------
+# to be nested within Graph class
 class Vertex:
     '''Lightweight vertex structure for a graph.'''
     __slot__ = '_element'
@@ -14,3 +15,65 @@ class Vertex:
 
     def element(self):
         '''Return element associated with this vertex.'''
+        pass
+
+    def __hash__(self):
+        pass
+
+# ---------------- nested Edge class ----------------
+# to be nested within Graph class
+class Edge:
+    '''Lightweight edge structure for a graph.'''
+    __slot__ = 'origin', 'destination', 'element'
+
+    def __init__(self, u, v, x):
+        pass
+
+    def endpoints(self):
+        pass
+
+    def opposite(self, v):
+        pass
+
+    def element(self):
+        pass
+
+    def __hash__(self):
+        pass
+
+# ---------------- Graph class ----------------
+class Graph:
+    '''Representation of a simple graph using an adjacency map.'''
+
+    def __init__(self, directed=False):
+        pass
+
+    def is_directed(self):
+        pass
+
+    def vertex_count(self):
+        pass
+
+    def vertices(self):
+        pass
+
+    def edge_count(self):
+        pass
+
+    def edges(self):
+        pass
+
+    def get_edges(self, u, v):
+        pass
+
+    def degree(self, v, outgoing=True):
+        pass
+
+    def incident_edges(self, v, outgoing=True):
+        pass
+
+    def insert_vertex(self, x=None):
+        pass
+
+    def insert_edge(self, u, v, x=None):
+        pass
