1a9f467ea70f0b7bc1cb5fe289559df3894e8118
ryan_shiroma@hotmail.com <ryan_shiroma@hotmail.com>
2020-09-30 20:16:41 -0300

    Added files via upload

--- Graph_Algo/graph_ADT.py
+++ Graph_Algo/graph_ADT.py
@@ -15,31 +15,37 @@ class Vertex:
 
     def element(self):
         '''Return element associated with this vertex.'''
-        pass
+        return self._element
 
-    def __hash__(self):
-        pass
+    def __hash__(self):         # will allow vertex to be a map key
+        return hash(id(self))
 
 # ---------------- nested Edge class ----------------
 # to be nested within Graph class
 class Edge:
     '''Lightweight edge structure for a graph.'''
-    __slot__ = 'origin', 'destination', 'element'
+    __slot__ = '_origin', '_destination', '_element'
 
     def __init__(self, u, v, x):
-        pass
+        '''Do not call constructor directly. Use Graph's insert_edge(u,v,x).'''
+        self._origin = u
+        self._destination = v
+        self._element = x
 
     def endpoints(self):
-        pass
+        '''Return (u,v) tuple for vertices u and v.'''
+        return(self._origin, self._destination)
 
     def opposite(self, v):
-        pass
+        '''Return the vertex that is opposite v on this edge.'''
+        return self._destination if v is self.origin else self.origin
 
     def element(self):
-        pass
+        '''Return element associated with this edge.'''
+        return self._element
 
-    def __hash__(self):
-        pass
+    def __hash__(self):         # will allow edge to be a map key
+        return hash( (self._origin, self._destination) )
 
 # ---------------- Graph class ----------------
 class Graph:
