49387ae887406199e17f926a8b4fbcf463e78a19
ryan_shiroma@hotmail.com <ryan_shiroma@hotmail.com>
2020-09-30 21:28:19 -0300

    Added files via upload

--- Graph_Algo/graph_ADT.py
+++ Graph_Algo/graph_ADT.py
@@ -52,34 +52,73 @@ class Graph:
     '''Representation of a simple graph using an adjacency map.'''
 
     def __init__(self, directed=False):
-        pass
+        '''Create an empty graph (undirected, by default).
+
+        Graph is directed if optional parameter is set to True.
+        '''
+        self.outgoing = {}
+        # only create second map for directed graph; use alias for directed
+        self._incoming = {} if directed else self._outgoing
 
     def is_directed(self):
-        pass
+        '''Return True if this is a directed graph; False if undirected.
+
+        Property is based on the original declaration  of the graph, not its contents.
+        '''
+        return self._incoming is not self._outgoing  # directed if maps are distinct
 
     def vertex_count(self):
-        pass
+        '''Return the number of vertices in the graph.'''
+        return len(self._outgoing)
 
     def vertices(self):
-        pass
+        '''Return an iteration of all vertices of the graph.'''
+        return self._outgoing.keys()
 
     def edge_count(self):
-        pass
+        '''Return the number of edges in the graph.'''
+        total = sum(len(self._outgoing[v]) for v in self._outgoing)
+        # for undirected graphs, make sure not to double-count edges
+        return total if self.is_directed() else total // 2
 
     def edges(self):
-        pass
+        '''Return a set of all edges of the graph.'''
+        result = set()      # avoid double-reporting edges of undirected graph
+        for secondary_map in self._outgoing.values():
+            result.update(secondary_map.values())   # add edges to resulting set
+        return result
 
     def get_edges(self, u, v):
-        pass
+        '''Return the edge from u to v, or None if not adjacent.'''
+        return self._outgoing[u].get(v)     # returns None if v not adjacent
 
     def degree(self, v, outgoing=True):
-        pass
+        '''Return number of (outgoing) edges incident to vertex v in the graph.
+
+        If graph is directed, optional parameter used to count incoming edges.
+        '''
+        adj = self._outgoing if outgoing else self._incoming
+        return len(adj[v])
 
     def incident_edges(self, v, outgoing=True):
-        pass
+        '''Return all (outgoing) edges incident to vertex v in the graph.
+
+        If graph is directed, optional parameter used to request incoming edges.
+        '''
+        adj = self._outgoing if outgoing else self._incoming
+        for edge in adj[v].values():
+            yield edge
 
     def insert_vertex(self, x=None):
-        pass
+        '''Insert and return a new Vertex with element x.'''
+        v = self.Vertex(x)
+        self._outgoing[v] = {}
+        if self.is_directed():
+            self._incoming[v] = {}      # need distinct map for incoming edges
+        return v
 
     def insert_edge(self, u, v, x=None):
-        pass
+        '''Insert and return a new Edge from u to v with auxiliary element x.'''
+        e = self.Edge(u, v, x)
+        self._outgoing[u][v] = e
+        self._incoming[v][u] = e
